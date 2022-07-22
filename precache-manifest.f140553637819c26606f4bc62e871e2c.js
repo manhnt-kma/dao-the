141df549054f5f64e92b1e6bc4a458496d17801b
Nguyễn Tiến Mạnh <nguyentienmanh11297@gmail.com>
2022-07-22 21:40:40 +0700

    Updates

---
+++ precache-manifest.f140553637819c26606f4bc62e871e2c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "798d8a672606dd20dbd6",
+    "url": "/dao-the/static/css/main.cbf045b7.chunk.css"
+  },
+  {
+    "revision": "798d8a672606dd20dbd6",
+    "url": "/dao-the/static/js/main.798d8a67.chunk.js"
+  },
+  {
+    "revision": "8c98aab82769540efb62",
+    "url": "/dao-the/static/js/1.8c98aab8.chunk.js"
+  },
+  {
+    "revision": "8bc9dae54c1ba2ba0a68",
+    "url": "/dao-the/static/js/2.8bc9dae5.chunk.js"
+  },
+  {
+    "revision": "69d9798c502fa6c75ea0",
+    "url": "/dao-the/static/js/runtime~main.69d9798c.js"
+  },
+  {
+    "revision": "aca2dabe50a3b9c2feb69bf4b4e6bc46",
+    "url": "/dao-the/static/media/01.aca2dabe.png"
+  },
+  {
+    "revision": "d8afa23ea12b9af1e2717de80bf44ba4",
+    "url": "/dao-the/static/media/02.d8afa23e.png"
+  },
+  {
+    "revision": "248baf33e46fa5501b93c4a7879b52a8",
+    "url": "/dao-the/static/media/zalo-icon.248baf33.png"
+  },
+  {
+    "revision": "433ba0a0ebcdf6d96db19269a888e499",
+    "url": "/dao-the/static/media/messenger-icon.433ba0a0.png"
+  },
+  {
+    "revision": "6e873cd876abe5d4433ce1dc5fd51ed3",
+    "url": "/dao-the/static/media/phone.6e873cd8.png"
+  },
+  {
+    "revision": "0083712a91f7833311093a447a5c96ba",
+    "url": "/dao-the/static/media/background-footer.0083712a.jpg"
+  },
+  {
+    "revision": "cb4371064d5b7956786764f506a91b77",
+    "url": "/dao-the/static/media/contactus-icon.cb437106.png"
+  },
+  {
+    "revision": "f6bf36620a52387001e179f067933664",
+    "url": "/dao-the/index.html"
+  }
+];
